255fef8d22e77db0e9321f82466c3c269f888a4a
Anowar1987 <anowarhossen.iim@gmail.com>
2024-03-13 21:10:45 +0600

    population and area updated

--- src/Components/Countries/Country/Country.jsx
+++ src/Components/Countries/Country/Country.jsx
@@ -1,10 +1,14 @@
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    
+    const {name, flags, population, area, capital} = country;
     return (
         <div className='country'>
             <h3>Name: {name.common}</h3>
+            <p>Capital: {capital}</p>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Land Area: {area}</p>
         </div>
     );
 };
